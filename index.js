19b26a830c8746ac76f17c8418b4d071c2bbed70
yos10 <61726023+yos10@users.noreply.github.com>
2021-04-17 22:34:52 +0900

    アンケートで名前も入力できるように

--- index.js
+++ index.js
@@ -1,38 +1,49 @@
 'use strict';
 const http = require('http');
-const server = http.createServer((req, res) => {
-  const now = new Date();
-  console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
-  res.writeHead(200, {
-    'Content-Type': 'text/html; charset=utf-8'
-  });
+const qs = require('querystring');
 
-  switch (req.method) {
-    case 'GET':
-      const fs = require('fs');
-      const rs = fs.createReadStream('./form.html');
-      rs.pipe(res);
-      break;
-    case 'POST':
-      let rawData = '';
-      req.on('data', (chunk) => {
-        rawData = rawData + chunk;
-      }).on('end', () => {
-        const decoded = decodeURIComponent(rawData);
-        console.info('[' + now + '] 投稿: ' + decoded);
-        res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
-          decoded + 'が投稿されました</h1></body></html>');
-        res.end();
-      });
-      break;
-    default:
-      break;
-  }
-}).on('error', (e) => {
-  console.error('[' + new Date() + '] Server Error', e);
-}).on('clientError', (e) => {
-  console.error('[' + new Date() + '] Client Error', e);
-});
+const server = http
+  .createServer((req, res) => {
+    const now = new Date();
+    console.info('[' + now + '] Requested by ' + req.socket.remoteAddress);
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8',
+    });
+
+    switch (req.method) {
+      case 'GET':
+        const fs = require('fs');
+        const rs = fs.createReadStream('./form.html');
+        rs.pipe(res);
+        break;
+      case 'POST':
+        let rawData = '';
+        req
+          .on('data', (chunk) => {
+            rawData = rawData + chunk;
+          })
+          .on('end', () => {
+            const answer = qs.parse(rawData);
+            // console.log(answer);
+            // console.log(answer['name']);
+            // console.log(answer['yaki-shabu']);
+            const body = `${answer['name']}さんは${answer['yaki-shabu']}に投票しました`;
+            console.info('[' + now + '] 投稿: ' + body);
+            const html = `<!DOCTYPE html><html lang="ja"><body><p>${body}</p></body></html>`;
+            res.write(html);
+            res.end();
+          });
+        break;
+      default:
+        break;
+    }
+  })
+  .on('error', (e) => {
+    console.error('[' + new Date() + '] Server Error', e);
+  })
+  .on('clientError', (e) => {
+    console.error('[' + new Date() + '] Client Error', e);
+  });
 const port = 8000;
 server.listen(port, () => {
   console.info('[' + new Date() + '] Listening on ' + port);
